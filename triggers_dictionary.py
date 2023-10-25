4bf22c576cf4b5fc6454e19eacb69d81a04d0b46
Braden Allmond <braden.allmond@gmail.com>
2023-10-25 18:53:07 +0200

    added electron triggers

--- triggers_dictionary.py
+++ triggers_dictionary.py
@@ -14,6 +14,12 @@ triggers_dictionary = {
              "HLT_IsoMu20_eta2p1_TightChargedIsoPFTauHPS27_eta2p1_TightID_CrossL1", # Run2, only for Era F study
              "HLT_IsoMu20_eta2p1_LooseDeepTauPFTauHPS27_eta2p1_CrossL1", 
           ],
+  "etau"  : [
+             "HLT_Ele30_WPTight_Gsf",
+             "HLT_Ele32_WPTight_Gsf",
+             "HLT_Ele35_WPTight_Gsf",
+             "HLT_Ele24_eta2p1_WPTight_Gsf_LooseDeepTauPFTauHPS30_eta2p1_CrossL1",
+          ],
   "dimuon": [
              "HLT_IsoMu24",
              "HLT_IsoMu27",
