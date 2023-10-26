28a5b4c6f32019ed52cccd897d7d4b10b5a2086c
Braden Allmond <braden.allmond@gmail.com>
2023-10-26 12:34:14 +0200

    corrected mutau cross-trigger selection

--- triggers_dictionary.py
+++ triggers_dictionary.py
@@ -4,15 +4,15 @@
 
 triggers_dictionary = {
   "ditau" : [
+             "HLT_DoubleMediumDeepTauPFTauHPS35_L2NN_eta2p1",
              "HLT_DoubleMediumChargedIsoPFTauHPS40_Trk1_eta2p1", # Run2, only for Era F study
              "HLT_DoubleTightChargedIsoPFTauHPS35_Trk1_eta2p1",  # Run2, only for Era F study
-             "HLT_DoubleMediumDeepTauPFTauHPS35_L2NN_eta2p1",
           ],
   "mutau" : [
              "HLT_IsoMu24", 
              "HLT_IsoMu27",
-             "HLT_IsoMu20_eta2p1_TightChargedIsoPFTauHPS27_eta2p1_TightID_CrossL1", # Run2, only for Era F study
              "HLT_IsoMu20_eta2p1_LooseDeepTauPFTauHPS27_eta2p1_CrossL1", 
+             "HLT_IsoMu20_eta2p1_TightChargedIsoPFTauHPS27_eta2p1_TightID_CrossL1", # Run2, only for Era F study
           ],
   "etau"  : [
              "HLT_Ele30_WPTight_Gsf",
